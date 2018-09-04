2786c101da0cfcf38226d84c1202cbc752a94917
bhlong <bhlong@edu.uwaterloo.ca>
2018-09-04 17:01:09 -0400

    implemented user register and login routes

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://jason:jason123JKLJKL@ds143932.mlab.com:43932/devconnect"
+  mongoURI: "mongodb://jason:jason123JKLJKL@ds143932.mlab.com:43932/devconnect",
+  secretOrKey: "secret"
 };
