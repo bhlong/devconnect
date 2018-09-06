d1ac6d00b7cb0b5d75cc50ca51dca7e048470a44
bhlong <bhlong@edu.uwaterloo.ca>
2018-09-05 23:38:32 -0400

    partially added register validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,6 +5,9 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const passport = require("passport");
 
+// Load Input Validation
+const validateRegisterInput = require("../../validation/register");
+
 // Load UserSchema
 const User = require("../../models/User");
 const keys = require("../../config/keys");
@@ -18,9 +21,17 @@ router.get("/test", (req, res) => res.json({ msg: "Users works" }));
 // @desc    Register user
 // @access  Public
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: "Email already exists" });
+      errors.email = "Email already exists";
+      return res.status(400).json({ errors });
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200", //size
