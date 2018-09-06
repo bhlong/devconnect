56ad8b30230dbce9d1d1062f0640344220b75928
bhlong <bhlong@edu.uwaterloo.ca>
2018-09-05 23:07:37 -0400

    implemented jwt and corresponding passport strategy

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const passport = require("passport");
 
 // Load UserSchema
 const User = require("../../models/User");
@@ -85,5 +86,20 @@ router.post("/login", (req, res) => {
     });
   });
 });
+
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const app = express();
 
@@ -20,7 +21,11 @@ mongoose
   .then(() => console.log("MongoDB connected."))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello world"));
+// Passport middleware
+app.use(passport.initialize());
+
+// Password Config
+require("./config/passport")(passport);
 
 // Use routes
 app.use("/api/users", users);
