b5b76d234d3ac50f44155009f48c3a8f7f46ff43
bhlong <bhlong@edu.uwaterloo.ca>
2018-09-08 11:35:15 -0400

    added validation to profile update and create

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
 
+// Load Validation
+const validateProfileInput = require("../../validation/profile");
+
 // Load Profile Model
 const Profile = require("../../models/Profile");
 
@@ -23,6 +26,7 @@ router.get(
   (req, res) => {
     const errors = {};
     Profile.findOne({ user: req.user.id })
+      .populate("user", ["name", "avatar"])
       .then(profile => {
         if (!profile) {
           errors.noprofile = "There is no profile for this user";
@@ -41,6 +45,13 @@ router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    const { errors, isValid } = validateProfileInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
     // Get fields
     const profileFields = {};
 
@@ -62,6 +73,7 @@ router.post(
 
     const inputData = Object.keys(req.body);
 
+    profileFields.social = {};
     for (key of inputData) {
       if (whiteList.includes(key)) {
         if (key === "skills" && typeof req.body.skills !== "undefined") {
