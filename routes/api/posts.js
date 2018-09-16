bebd7dede8f4cb019d595835e5ac7867d0100ff3
bhlong <bhlong@edu.uwaterloo.ca>
2018-09-16 15:00:39 -0400

    added post routes to get all, get by id, delete by id, like by id, and unlike by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,6 +6,9 @@ const passport = require("passport");
 // Post model
 const Post = require("../../models/Post");
 
+// Profile model
+const Profile = require("../../models/Profile");
+
 // Validation
 const validatePostInput = require("../../validation/post");
 
@@ -13,6 +16,27 @@ const validatePostInput = require("../../validation/post");
 // @desc    Tests posts route
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "Posts works" }));
+
+// @route   GET api/posts/:id
+// @desc    Get post by id
+// @access  Public
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(404).json({ nopostfound: "No post found with that ID" })
+    );
+});
+
+// @route   GET api/posts
+// @desc    Get posts
+// @access  Public
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ nopostsfound: "No posts found" }));
+});
 
 // @route   POST api/posts
 // @desc    Create post
@@ -36,6 +60,78 @@ router.post(
       user: req.user.id
     });
     newPost.save().then(post => res.json(post));
+  }
+);
+
+// @route   DELETE api/posts/:id
+// @desc    Delete post
+// @access  Private
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    // Check Id of owner post
+    Post.findOneAndRemove({ _id: req.params.id, user: req.user.id })
+      .then(post => {
+        return !post
+          ? res.status(401).json({ post: "post not found" })
+          : res.status(200).json({ post: "post deleted" });
+      })
+      .catch(err =>
+        res.status(404).json({ post: "It had a problem deleting the post" })
+      );
+  }
+);
+
+// @route   POST api/posts/like/:id
+// @desc    Like post
+// @access  Private
+router.post(
+  "/like/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        if (
+          post.likes.filter(like => like.user.toString() === req.user.id)
+            .length > 0
+        ) {
+          return res
+            .status(400)
+            .json({ alreadyliked: "User already liked this post" });
+        } else {
+          // Add user id to likes array
+          post.likes.unshift({ user: req.user.id });
+          post.save().then(post => res.json(post));
+        }
+      })
+      .catch(err => res.status(404).json({ postnotfound: "No posts found" }));
+  }
+);
+
+// @route   POST api/posts/unlike/:id
+// @desc    Unlike post
+// @access  Private
+router.post(
+  "/unlike/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        const likeIndex = post.likes.findIndex(
+          like => like.user.toString() === req.user.id
+        );
+        if (likeIndex === -1) {
+          return res
+            .status(400)
+            .json({ notliked: "You have not yet liked this post" });
+        } else {
+          // Add user id to likes array
+          post.likes.splice(likeIndex, 1);
+          post.save().then(post => res.json(post));
+        }
+      })
+      .catch(err => res.status(404).json({ postnotfound: "No posts found" }));
   }
 );
 
